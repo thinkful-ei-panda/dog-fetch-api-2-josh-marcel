f49f1495f27734e380f9ca243605d041487cf7c1
Marcel Woodman <woodman_marcel@yahoo.com>
2020-06-09 12:53:54 -0700

    finished select breed app

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 'use strict';
-
+const log = console.log
 // this function fetches the dog images
 function getDogImages() {
-  const fetchNum = $('#numberOfDogs').val(),
-    fetchUrl = `https://dog.ceo/api/breeds/image/random/${fetchNum}`;
+  const breed = $('#typeOfBreed').val(),
+    fetchUrl = `https://dog.ceo/api/breed/${breed}/images/random`;
+    
   fetch(fetchUrl)
-    .then(response => response.json())
+    .then(response => response.json()) 
     .then(responseJson => displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .catch(error => log('We couldn"t find that breed. Please try another breed name.'));
+    
 }
 
 // this function generates the results
 function generateResults(responseJson) {
-  const dogImgs = [];
-  for (let i = 0; i<responseJson.message.length; i++) {
-    dogImgs.push(`<img src="${responseJson.message[i]}" class="results-img"></img>`);
-  }
-  return dogImgs;
+  const dogImg = [];
+  // for (let i = 0; i<responseJson.message.length; i++) {
+    dogImg.push(`<img src="${responseJson.message}" class="results-img"></img>`);
+  // }
+  return dogImg;
 }
 
 // this function displays the results
 function displayResults(responseJson) {
-  const html = generateResults(responseJson).join('');
+  log(responseJson.status)
+  if(responseJson.status === 'error') {
+    alert('We couldnt find that breed. Please try another breed name.')
+  }
+  else {const html = generateResults(responseJson).join('');
   $('#dogs').html(html);
   $('.results').removeClass('hidden');
+}
 }
 
 // this function creates the watch form
